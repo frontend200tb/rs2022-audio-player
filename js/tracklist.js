958c14a0d06f98856b92f5362019507dd7bb4927
Иван Каширин <85587225+frontend200tb@users.noreply.github.com>
2022-03-07 23:47:41 +0300

    Add files via upload

---
+++ js/tracklist.js
@@ -0,0 +1,26 @@
+const tracks = [
+
+  {
+    src: './assets/audio/1990 Tajci - Moj Mali Je Opasan.mp3',
+    name: `Moj Mali Je Opasan`,
+    artist: 'Tajci',
+    img: './assets/img/tajci.jpg',
+  },
+
+  {
+    src: './assets/audio/2000 Lidija - Nisam te birala ja.mp3',
+    name: `Nisam te birala ja`,
+    artist: 'Lidija',
+    img: './assets/img/lidija.jpg',
+  },
+
+  {
+    src: './assets/audio/2016 Sanja Djuric - Ne Da Mi Se Da Se Zaljubim.mp3',
+    name: `Ne Da Mi Se Da Se Zaljubim`,
+    artist: 'Sanja Djuric',
+    img: './assets/img/sanja djuric.jpg',
+  },
+
+];
+
+export default tracks;
